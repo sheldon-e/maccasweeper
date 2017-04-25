31d6d60013b87efa9abfed612554816a3d84cc23
Sheldon Evans <sheldonevans92@yahoo.co.jp>
2017-04-25 07:17:27 -0500

    Added feature for touch devices
    I think it’ll probably break the game but whatever!

--- assets/js/macca.js
+++ assets/js/macca.js
@@ -107,6 +107,42 @@ var gameTracker = "no-track";  // initial value
             }
       })
   };
+
+
+  //simulate right click with long tap : for touch devices
+  (function() {
+    $.fn.longTap = function(options) {
+        
+        options = $.extend({
+            delay: 1000,
+            onRelease: null
+        }, options);
+        
+        var eventType = {
+            mousedown: 'ontouchstart' in window ? 'touchstart' : 'mousedown',
+            mouseup: 'ontouchend' in window ? 'touchend' : 'mouseup'
+        };
+        
+        return this.each(function() {
+            $(this).on(eventType.mousedown + '.longtap', function() {
+                $(this).data('touchstart', +new Date);
+            })
+            .on(eventType.mouseup + '.longtap', function(e) {
+                var now = +new Date,
+                    than = $(this).data('touchstart');
+                now - than >= options.delay && options.onRelease && options.onRelease.call(this, e);
+            });
+        });
+    };
+})(jQuery);
+
+$('.board-container').longTap({
+    delay: 1000, // really long tap
+    onRelease: function(e) {
+        c($(this).position(), e);
+        //game.clickSquare();
+    }
+});
 // End of incantation
 
 var board = {
